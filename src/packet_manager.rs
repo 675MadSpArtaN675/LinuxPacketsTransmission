6e4cae09b7fee7fa1ddebccc51484d2a644edae4
MadSpartan <va44l@yandex.ru>
2026-08-02 16:37:32 +0300

    Start creating a command executor

--- src/packet_manager.rs
+++ src/packet_manager.rs
@@ -5,7 +5,32 @@ pub mod utility {
     }
 }
 
-pub struct CommandOfPacketManager {
+mod packet_manager_trait {
+    use super::utility::PacketManagerResultCode;
+
+    pub trait PacketManager {
+        fn install(&self, packets: &Vec<String>) -> PacketManagerResultCode;
+        fn remove(&self, packets: &Vec<String>) -> PacketManagerResultCode;
+
+        fn update(&self) -> PacketManagerResultCode;
+        fn show_updates(&self) -> Vec<String>;
+        fn update_system(&self) -> PacketManagerResultCode;
+
+        fn repos(&self) -> Vec<String>;
+        fn add_repo(&self, repo_name: &str, repo_url: &str);
+        fn remove_repo(&self, repo_name: &str);
+
+        fn search(&self, packets: Vec<String>) -> Vec<String>;
+        fn applications_list(&self) -> Vec<String>;
+    }
+}
+
+use packet_manager_trait::PacketManager;
+use std::process::{Command, Child, Stdio};
+
+use crate::packet_manager::utility::PacketManagerResultCode;
+
+pub struct PacketManagerCommand {
     basic_command: String,
     install_command: String,
     remove_command: String,
@@ -14,9 +39,9 @@ pub struct CommandOfPacketManager {
     update_command: String,
 }
 
-impl CommandOfPacketManager {
-    pub fn new_empty(base_command_name: String) -> CommandOfPacketManager {
-        let command_obj: CommandOfPacketManager = CommandOfPacketManager{
+impl PacketManagerCommand {
+    pub fn new_empty(base_command_name: String) -> PacketManagerCommand {
+        let command_obj: PacketManagerCommand = PacketManagerCommand{
             basic_command: base_command_name,
             install_command: String::new(),
             remove_command: String::new(),
@@ -28,13 +53,13 @@ impl CommandOfPacketManager {
         return command_obj;
     }
 
-    pub fn new(base_command_name: String) -> CommandOfPacketManager {
+    pub fn new(base_command_name: String) -> PacketManagerCommand {
         return get_packet_manager_preset(base_command_name);
     }
 }
 
-fn get_packet_manager_preset(base_command_name: String) -> CommandOfPacketManager {
-    let mut command_obj: CommandOfPacketManager = CommandOfPacketManager::new_empty(base_command_name.clone());
+fn get_packet_manager_preset(base_command_name: String) -> PacketManagerCommand {
+    let mut command_obj: PacketManagerCommand = PacketManagerCommand::new_empty(base_command_name.clone());
 
     match base_command_name.as_str() {
         "zypper" => {
@@ -56,7 +81,7 @@ fn get_packet_manager_preset(base_command_name: String) -> CommandOfPacketManage
     return command_obj;
 }
 
-fn create_standard_commands(command_obj: &mut CommandOfPacketManager) {
+fn create_standard_commands(command_obj: &mut PacketManagerCommand) {
     command_obj.install_command = String::from("install");
     command_obj.remove_command = String::from("remove");
     command_obj.search_command = String::from("search");
@@ -65,22 +90,60 @@ fn create_standard_commands(command_obj: &mut CommandOfPacketManager) {
 }
 
 
-mod packet_manager_trait {
-    use super::utility::PacketManagerResultCode;
+pub struct PacketManagerCommandExecutor
+{
+    command_obj: PacketManagerCommand
+}
 
-    pub trait PacketManager {
-        fn install(self, packets: Vec<String>) -> PacketManagerResultCode;
-        fn remove(self, packets: Vec<String>) -> PacketManagerResultCode;
+impl PacketManager for PacketManagerCommandExecutor {
+    fn install(&self, packets: &Vec<String>) -> PacketManagerResultCode {
+        let full_command: String = format!("{} {}", self.command_obj.basic_command, self.command_obj.install_command);
 
-        fn update(self) -> PacketManagerResultCode;
-        fn show_updates(self) -> Vec<String>;
-        fn update_system(self) -> PacketManagerResultCode;
+        let mut _install_command: Command = Command::new(full_command);
+        _install_command.args(packets);
+        let mut install_process: Child = _install_command.spawn().expect("Error of installer launch...");
 
-        fn repos(self) -> Vec<String>;
-        fn add_repo(self, repo_name: &str, repo_url: &str);
-        fn remove_repo(self, repo_name: &str);
+        let _out_stream = install_process.stdout.take();
+        let _err_stream = install_process.stderr.take();
 
-        fn search(self, packets: Vec<String>) -> Vec<String>;
-        fn applications_list(self) -> Vec<String>;
+        let _status_code = install_process.wait();
+        if _status_code.is_ok() && _out_stream.is_some() && _err_stream.is_some() {
+        }
+
+    }
+    fn remove(self, packets: Vec<String>) -> PacketManagerResultCode {
+
+    }
+
+    fn update(self) -> PacketManagerResultCode {
+
+    }
+
+    fn show_updates(self) -> Vec<String> {
+
+    }
+
+    fn update_system(self) -> PacketManagerResultCode {
+
+    }
+
+    fn repos(self) -> Vec<String> {
+
+    }
+
+    fn add_repo(self, repo_name: &str, repo_url: &str) {
+
+    }
+
+    fn remove_repo(self, repo_name: &str) {
+
+    }
+
+    fn search(self, packets: Vec<String>) -> Vec<String> {
+
+    }
+
+    fn applications_list(self) -> Vec<String> {
+
     }
 }
